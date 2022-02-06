a14c11f8f3d039541f299cfb005100fb21ec509b
SmallStall <36023330+winuwi@users.noreply.github.com>
2022-02-07 08:02:11 +0900

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,28 @@
+import { useLoaderData } from "remix";
+import type { LoaderFunction } from "remix";
+import { createClient } from "@supabase/supabase-js";
+
+type Message = {
+  title: string;
+};
+
+export const loader: LoaderFunction = async () => {
+  const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    fetch: (...args) => fetch(...args),
+  });
+  const { data } = await supabase
+    .from<Message>("message")
+    .select("title");
+  return data;
+};
+
+export default function Index() {
+  const messages = useLoaderData<Message[]>();
+  return (
+    <div>
+      {messages.map((message) => (
+        <h1>{message.title}</h1>
+      ))}
+    </div>
+  );
+}
---
+++ bindings.ts
@@ -0,0 +1,6 @@
+export {};
+
+declare global {
+  const SUPABASE_ANON_KEY: string;
+  const SUPABASE_URL: string;
+}
