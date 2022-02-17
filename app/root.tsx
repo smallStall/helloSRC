f27cf14642c8668636b3cacf9439685b42a66eca
smallStall <91565962+smallStall@users.noreply.github.com>
2022-02-17 12:33:59 +0900

    Update root.tsx
    <Scripts />を追記。

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "remix";
+import { useLoaderData, Scripts } from "remix";
 import type { LoaderFunction } from "remix";
 import { createClient } from "@supabase/supabase-js";
 
@@ -10,19 +10,22 @@ export const loader: LoaderFunction = async () => {
   const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     fetch: (...args) => fetch(...args),
   });
-  const { data } = await supabase
-    .from<Message>("message")
-    .select("title");
+  const { data } = await supabase.from<Message>("message").select("title");
   return data;
 };
 
 export default function Index() {
   const messages = useLoaderData<Message[]>();
   return (
-    <div>
-      {messages.map((message) => (
-        <h1>{message.title}</h1>
-      ))}
-    </div>
+    <html lang="jp">
+      <body>
+        <div>
+          {messages.map((message) => (
+            <h1>{message.title}</h1>
+          ))}
+          <Scripts />
+        </div>
+      </body>
+    </html>
   );
 }
